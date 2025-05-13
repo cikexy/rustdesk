12ae9902273eff07a6597391cd87ecab0ec05140
cikexy <695348701@qq.com>
2025-05-14 00:40:46 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -956,7 +956,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rd.ddmzf.cn:7000".to_owned()
+    "http://rd.ddmzf.cn:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
